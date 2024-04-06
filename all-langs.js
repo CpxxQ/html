38d45cc4fdca2423368d77ba30b2aa7085c5e416
CpxxQ <113293521+CpxxQ@users.noreply.github.com>
2024-04-06 13:06:08 +0200

    Add files via upload

---
+++ all-langs.js
@@ -0,0 +1,4 @@
+const home_btn = document.getElementById('home-btn')
+home_btn.addEventListener('click', function() {
+    window.location.href = "index.html"
+});
---
+++ script.js
@@ -0,0 +1,4 @@
+const all_languages_btn = document.getElementById('all-languages-btn')
+all_languages_btn.addEventListener('click', function() {
+    window.location.href = 'all-langs.html'
+});
